4dce78c138eb440d4b7e1f5d8a743660fefb2912
ElieGehin <etgehin@gmail.com>
2021-01-08 22:44:03 +0100

    Added Javascript

---
+++ Formulaire.js
@@ -0,0 +1,17 @@
+var canards ="Picsou";
+var chiffredAffaire = 180000000;
+chiffredAffaire = 1900000 + "K";
+var pognon ="Fortune : " + chiffredAffaire;
+var devise = " $";
+console.log( canards + "\n" + pognon + devise);
+
+var fortunePicsou= 2000000000;
+var fortuneGripsou= 1000000000;
+var concoursFortune= fortunePicsou !== fortuneGripsou;
+console.log(concoursFortune);
+
+var picsouvainqueur= true;
+var picsouWin= true;
+
+console.log("Picsou vainqueur = " + picsouvainqueur);
+console.log("Victoire ! " + picsouWin);
